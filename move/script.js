c05516313bd6a3f22e93f9543e72cec0b0512537
PochWar <ripoche.b@gmail.com>
2017-03-19 15:47:35 +0100

    Add "run" mode in "move" project

--- move/script.js
+++ move/script.js
@@ -1,23 +1,43 @@
-document.querySelector('.square').style.top = "150px";
+//on définit les proprietés CSS "top" et left" du carré
+document.querySelector('.square').style.top = "170px";
 document.querySelector('.square').style.left = "150px";
 
-function moveIt(direction){
+function moveIt(direction,distance){
     switch (direction){
         case "top" :
-            var incr = -5;
             var property = "top"
+            var negative = true
             break;
 
         case "bottom" :
-            var incr = 5;
             var property = "top"
+            var negative = false
+            break;
+
+        case "left" :
+            var property = "left"
+            var negative = true
+            break;
+
+        case "right" :
+            var property = "left"
+            var negative = false
             break;
     }
     var coord = document.querySelector('.square').style[property];
     coord = coord.replace("px", "");
     coord = parseInt(coord);
-    if (coord > 0){
-        coord += incr;
+    //top & left
+    if (negative){
+        if (coord > 0){
+            coord -= distance;
+        }
+    }
+    //bottom & right
+    if(!negative){
+        if (coord < 300){
+            coord += distance;
+        }
     }
     coord = coord + "px";
     document.querySelector('.square').style[property] = coord;
@@ -25,43 +45,16 @@ function moveIt(direction){
 
 
 function move(e) {
-    switch (e.keyCode){
-        //haut
-        case 38 :
-            moveIt("top");
-            break;
-
-        //bas
-        case 40 :
-            moveIt("bottom");
-            break;
-
-        //gauche
-        case 37 :
-            var left = document.querySelector('.square').style.left;
-            var leftValue = left.replace("px", "");
-            leftValue = parseInt(leftValue);
-            if (leftValue > 0){
-                leftValue -= 5;
-            }
-            leftValue = leftValue + "px";
-            document.querySelector('.square').style.left = leftValue;
-            break;
-
-        //droite
-        case 39 :
-            var left = document.querySelector('.square').style.left;
-            var leftValue = left.replace("px", "");
-            leftValue = parseInt(leftValue);
-            if (leftValue < 300){
-                leftValue += 5;
-            }
-            leftValue = leftValue + "px";
-            document.querySelector('.square').style.left = leftValue;
-            break;
-
-        default :
-            console.log('toto');
-            break;
-    }
+    //top
+    if (e.keyCode == 38 && !e.shiftKey) {moveIt("top", 5);}
+    if (e.keyCode == 38 && e.shiftKey) {moveIt("top", 10);}
+    //bottom
+    if (e.keyCode == 40 && !e.shiftKey) {moveIt("bottom", 5);}
+    if (e.keyCode == 40 && e.shiftKey) {moveIt("bottom", 10);}
+    //left
+    if (e.keyCode == 37 && !e.shiftKey) {moveIt("left", 5);}
+    if (e.keyCode == 37 && e.shiftKey) {moveIt("left", 10);}
+    //right
+    if (e.keyCode == 39 && !e.shiftKey) {moveIt("right", 5);}
+    if (e.keyCode == 39 && e.shiftKey) {moveIt("right", 10);}
 }
