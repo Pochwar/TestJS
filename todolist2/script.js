638492911ce963fd917adcacb0df3617b6ab04a3
PochWar <ripoche.b@gmail.com>
2017-03-20 15:04:22 +0100

    Add css in "todolist2" project

--- todolist2/script.js
+++ todolist2/script.js
@@ -9,36 +9,48 @@ function addElement(){
         //création d'un nouvel element <p>
         var newP = document.createElement("P");
 
-        //insertion de l'objet texte dans le <p>
-        newP.appendChild(textnode);
+        //creation du span contenant le texte et la checkbox
+        var span1 = document.createElement("span");
 
         //creation de la checkbox "done"
         var checkDone = document.createElement("input");
         checkDone.type = 'checkbox';
         checkDone.className = 'done';
         checkDone.addEventListener('click', doneTask);
-        newP.insertBefore(checkDone, textnode);
+        span1.appendChild(checkDone);
+
+        //insertion de l'objet texte dans le <p>
+        span1.appendChild(textnode);
+
+        //ajout du text et checkbox
+        newP.appendChild(span1);
+
+        //creation du span contenant les boutons
+        var span2 = document.createElement("span");
 
         //création et ajour du bouton de supression
         var buttonDelete = document.createElement("button");
         buttonDelete.innerHTML = "X";
         buttonDelete.className = "del";
         buttonDelete.addEventListener('click', removeTask);
-        newP.appendChild(buttonDelete);
+        span2.appendChild(buttonDelete);
 
         //création et ajour du bouton montée
         var buttonUp = document.createElement("button");
         buttonUp.innerHTML = "UP";
         buttonUp.className = "up";
         buttonUp.addEventListener('click', upTask);
-        newP.appendChild(buttonUp);
+        span2.appendChild(buttonUp);
 
         //création et ajour du bouton montée
         var buttonDown = document.createElement("button");
         buttonDown.innerHTML = "Down";
         buttonDown.className = "down";
         buttonDown.addEventListener('click', downTask);
-        newP.appendChild(buttonDown);
+        span2.appendChild(buttonDown);
+
+        //ajout des boutons
+        newP.appendChild(span2);
 
         //récupération de la liste
         var list = document.querySelector('#list');
@@ -62,31 +74,35 @@ function validForm(e) {
     }
 }
 
-//decendre une tache
-function doneTask(e){
-    var text = e.target.parentElement;
-    var style = text.style.textDecoration;
-    if (style === "line-through"){text.style.textDecoration = "none";}
+//valider une tache
+function doneTask(){
+    var text = this.parentElement;
+    if (!this.checked){text.style.textDecoration = "none";}
     else {text.style.textDecoration = "line-through";}
 }
 
 //suppression d'une tache
-function removeTask(e){
-    e.target.parentElement.remove();
+function removeTask(){
+    this.parentElement.parentElement.remove();
 }
 
 //remonter une tache
-function upTask(e){
-    var parent = e.target.parentElement.parentElement;
-    var previous = e.target.parentElement.previousElementSibling;
-    parent.insertBefore(e.target.parentElement, previous);
+function upTask(){
+    var parent = this.parentElement.parentElement.parentElement;
+    var previous = this.parentElement.parentElement.previousElementSibling;
+    if(previous !== null){
+        parent.insertBefore(this.parentElement.parentElement, previous);
+    }
+
 }
 
 //decendre une tache
-function downTask(e){
-    var parent = e.target.parentElement.parentElement;
-    var next = e.target.parentElement.nextElementSibling.nextElementSibling;
-    parent.insertBefore(e.target.parentElement, next);
+function downTask(){
+    var parent = this.parentElement.parentElement.parentElement;
+    var next = this.parentElement.parentElement.nextElementSibling;
+    if(next !== null){
+        parent.insertBefore(next, this.parentElement.parentElement);
+    }
 }
 
 //trigger de la fonction addElement
