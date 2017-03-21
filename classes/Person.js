506149b47dbb2bb690a656671313937ca31ebdce
PochWar <ripoche.b@gmail.com>
2017-03-21 17:04:30 +0100

    Update classes project

--- classes/Person.js
+++ classes/Person.js
@@ -2,7 +2,14 @@ function Person(name, elementId){
     this.name = name;
     this.elementId = document.querySelector('#' + elementId);
     this.elementId.querySelector('.name').innerText = this.name;
+    this.bindEvents();
+
 }
+
+Person.prototype.bindEvents = function () {
+    this.elementId.querySelector('.sayHi').addEventListener("click", this.sayHi.bind(this));
+    this.elementId.querySelector('.sayBye').onclick = this.sayBye.bind(this);
+};
 
 Person.prototype.sayHi = function(){
     this.elementId.querySelector('.message').innerText = 'Bonjour je m\'apelle ' + this.name;
