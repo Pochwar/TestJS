1ffbb3a4bc8008c84b53791d4cf59b22f1163a81
PochWar <ripoche.b@gmail.com>
2017-03-27 17:00:29 +0200

    Nearly finish kittens memory

--- kittens/kittens.js
+++ kittens/kittens.js
@@ -5,6 +5,7 @@ function Kittens(images, params){
     this.height = this.params.height || 150;
     this.width = this.params.width || 150;
     this.createKittens();
+    this.imgShown = [];
 }
 
 Kittens.prototype.createKittens = function () {
@@ -27,49 +28,62 @@ Kittens.prototype.createKittens = function () {
         usedIndex.push(rand);
         i++;
     }
-
-
-
-
+    // cloneKittens.forEach(function(img){
+    //     console.log(img)
+    // });
     // creation des images
     cloneKittens.forEach(this.createImg.bind(this, parent));
-
-
-    // this.images.forEach((function(i){
-    //     this.createImg(parent, this, i);
-    //     // this.createImg(parent, this, i);
-    // }).bind(this));
 };
 
-
-Kittens.prototype.createImg = function (parent, image) {
-    var index = this.images.indexOf(image);
+//fonction de création des images
+Kittens.prototype.createImg = function (parent, image, id) {
+    var div = document.createElement("div");
+    div.addEventListener('click', this.show.bind(this, id));
+    div.setAttribute("style", "background:#eeeeee");
     var img = document.createElement("img");
     img.setAttribute("src", image);
     img.classeName = "img";
-    img.id = "img-" + index;
+    img.id = "img-" + id;
     img.setAttribute("height", this.height);
     img.setAttribute("width", this.width);
-    // img.addEventListener('click', reveal);
-    console.log(parent);
-    parent.append(img);
+    img.setAttribute("style", "visibility:hidden");
+    div.append(img);
+    parent.append(div);
 };
 
-Kittens.prototype.show = function () {
-    this.element.querySelector("#img").src = this.images[this.cursor];
-};
-
-
-
-Kittens.prototype.random = function () {
-    function randomize(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min +1)) + min;
+Kittens.prototype.show = function (id) {
+    if (this.imgShown.length < 2){
+        document.querySelector("#img-"+id).setAttribute("style", "visibility:visible");
+        this.imgShown.push(id);
     }
-    do {rand = randomize(0, this.images.length-1);}
-    while(rand === this.cursor);
-    this.cursor = rand;
-    this.show();
+    if (this.imgShown.length === 2){
+        var id0 = this.imgShown[0];
+        var id1 = this.imgShown[1];
+        if(document.querySelector("#img-"+id0).src !== document.querySelector("#img-"+id1).src){
+            setTimeout(function(){
+                this.hide(id0, id1);
+            }.bind(this), 1000);
+        }
+        this.imgShown = [];
+    }
 
 };
+
+Kittens.prototype.hide = function (id0,id1) {
+    document.querySelector("#img-"+id0).setAttribute("style", "visibility:hidden");
+    document.querySelector("#img-"+id1).setAttribute("style", "visibility:hidden");
+}
+
+
+// Kittens.prototype.random = function () {
+//     function randomize(min, max) {
+//         min = Math.ceil(min);
+//         max = Math.floor(max);
+//         return Math.floor(Math.random() * (max - min +1)) + min;
+//     }
+//     do {rand = randomize(0, this.images.length-1);}
+//     while(rand === this.cursor);
+//     this.cursor = rand;
+//     this.show();
+//
+// };
