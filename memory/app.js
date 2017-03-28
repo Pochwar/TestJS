7e4e833b7b887178227093961183206bca7810b0
PochWar <ripoche.b@gmail.com>
2017-03-28 16:33:44 +0200

    Delete memory project

--- memory/app.js
+++
@@ -1,6 +0,0 @@
-var img = ["img/1.jpg", "img/2.png", "img/3.jpg", "img/4.jpg", "img/5.png", "img/6.png", "img/7.png", "img/8.png"];
-
-// var meme = new Meme();
-// meme.create(img);
-Mememory.init('images', img);
-Mememory.init('images2', img);
